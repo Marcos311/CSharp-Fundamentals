8421b3becab1a960eebad1b67d794c9dcc8d3e64
Marcos_bat <marcos.vinisabino@gmail.com>
2021-08-03 13:31:12 -0300

    up

---
+++ CourseCSharp/CourseCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
